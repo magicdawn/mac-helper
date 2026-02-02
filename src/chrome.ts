83ead1cf41ed12e7995dc530d47de7e678fce69d
magicdawn <magicdawn@qq.com>
2026-02-02 13:32:21 +0800

    feat: add Brave browser support

--- src/chrome.ts
+++ src/chrome.ts
@@ -1,26 +1,47 @@
 import { run } from '@jxa/run'
 import type { GoogleChrome } from 'jxa-common-used'
+import { isAppRunning } from './app'
 
 export type ActiveTab = { url: string; title: string }
 
-export const Chrome = {
-  async getActiveTab(): Promise<ActiveTab> {
-    return await getChromeActiveTab()
-  },
+export enum BrowserAppName {
+  Chrome = 'Google Chrome',
+  Brave = 'Brave Browser',
 }
 
-export async function getChromeActiveTab(): Promise<ActiveTab> {
-  return await run(() => {
-    const app = Application<GoogleChrome>('Google Chrome')
-    app.includeStandardAdditions = true
+export class ChromiumBrowserHelper {
+  constructor(public appName: string) {}
+  isRunning = (): Promise<boolean> => isChromiumAppRunning(this.appName)
+  isFrontmost = (): Promise<boolean> => getChromiumIsFrontmost(this.appName)
+  getActiveTab = (): Promise<ActiveTab | undefined> => getChromiumActiveTab(this.appName)
+}
 
+export const Chrome = new ChromiumBrowserHelper(BrowserAppName.Chrome)
+export const Brave = new ChromiumBrowserHelper(BrowserAppName.Brave)
+
+/* #region implements */
+export function isChromiumAppRunning(appName: string = BrowserAppName.Chrome): Promise<boolean> {
+  return isAppRunning(appName)
+}
+export async function getChromiumActiveTab(appName: string = BrowserAppName.Chrome): Promise<ActiveTab | undefined> {
+  if (!(await isChromiumAppRunning(appName))) return
+  return await run((appName: string) => {
+    const app = Application<GoogleChrome>(appName)
+    app.includeStandardAdditions = true
     const win = (app.windows() || []).find((w) => !w.minimized())
     const tab = win?.activeTab()
     return {
       url: tab?.url(),
       title: tab?.title(),
     }
-  })
+  }, appName)
 }
-
-// getChromeActiveTab().then(console.log)
+export async function getChromiumIsFrontmost(appName: string = BrowserAppName.Chrome): Promise<boolean> {
+  if (!isChromiumAppRunning(appName)) return false
+  return await run((appName: string) => {
+    const app = Application<GoogleChrome>(appName)
+    app.includeStandardAdditions = true
+    return app.frontmost()
+  }, appName)
+}
+/* #endregion */
